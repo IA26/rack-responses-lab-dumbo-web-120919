ac4d9d5cbeef8763873531f6a5f8b6f3ed891a44
Isaac Avilez <isaac1180@aol.com>
2020-01-28 16:56:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,15 @@
 class Application
- 
+
   def call(env)
     resp = Rack::Response.new
-    
-    resp.write "Morning"
-    resp.write "Afternoon"
-      
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
     resp.finish
-    end 
- 
+  end
+
+end
 end
